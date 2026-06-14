6f1b2a510e5cff8f1f018cc80e515d01977a565a
potasium <potasium.1024@gmail.com>
2026-06-14 17:37:40 +0700

    telescope manager

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,31 +1,11 @@
 return {
 	{
-		"nvim-treesitter/nvim-treesitter",
-		lazy = false,
-		build = ":TSUpdate",
-		opts = {
-			auto_install = true,
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					-- init_selection = "<CR>",
-					scope_incremental = "<CR>",
-					node_incremental = "<TAB>",
-					node_decremental = "<S-TAB>",
-				},
-			},
-			highlight = {
-				enable = true,
-				sync_install = true,
-			},
-			indent = {
-				enable = true,
-			},
-		},
-		config = function(_, opts)
-			require("nvim-treesitter.install").compilers = { "clang", vim.fn.getenv("CC"), "cc", "gcc", "cl", "zig" }
-			require("nvim-treesitter.install").prefer_git = true
-			require("nvim-treesitter.configs").setup(opts)
+		"romus204/tree-sitter-manager.nvim",
+		dependencies = {}, -- tree-sitter CLI must be installed system-wide
+		config = function()
+			require("tree-sitter-manager").setup({
+				auto_install = true,
+			})
 		end,
 	},
 }
